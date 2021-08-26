fdc2e1f2ced55dbb188ea1871ee465100c9681cc
amartini01 <martini.alexandre@gmail.com>
2021-08-26 11:29:31 -0400

    added bert model

---
+++ src/tasks/task-5-modelling/bert_and_logistic_regression/bert_testing.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import DataLoader
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+
+df = pd.read_csv('../../data/twitter_hatespeech_dataset_after_cleaning.csv')
+pd.set_option('display.max_columns', None)
+print(df.head())
+
+df = df.iloc[:,[2,4]]
+df_x = df.drop('label', axis='columns')
+x_list = df_x['clean_tweet'].tolist()
+y_list = list(df.label)
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(x_list, y_list, test_size=0.1)
+
+from transformers import DistilBertTokenizerFast
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+train_dataset = IMDbDataset(train_encodings, train_labels)
+val_dataset = IMDbDataset(val_encodings, val_labels)
+
+# FINE_TUNING with TRAINER
+
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=1,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+    evaluation_strategy="epoch"
+)
+
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+import numpy as np
+from datasets import load_metric
+metric = load_metric("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset,             # evaluation dataset
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+trainer.evaluate()
+
+print("END")
--- src/tasks/task-5-modelling/bert_and_logistic_regression/log_reg.py
+++ src/tasks/task-5-modelling/bert_and_logistic_regression/log_reg.py
@@ -6,68 +6,11 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 
 nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
 import matplotlib.pyplot as plt
 import seaborn as sn
 
-# read dataset (downloaded from https://www.kaggle.com/vkrahul/twitter-hate-speech)
-df = pd.read_csv('../../data/train_E6oV3lV.csv')
-print(df.head())
 
-df.drop_duplicates(inplace=True)
-
-# remove @ from tweets
-clean_tweet_clm = []
-for _, row in df.iterrows():
-    s = row['tweet']
-    s_list = s.split()
-    tweet_list = []
-    for i in range(len(s_list)):
-        temp_str = s_list[i]
-        if not temp_str.startswith("@"):
-            tweet_list.append(temp_str)
-
-    clean_tweet_clm.append(' '.join(tweet_list))
-
-df['clean_tweet'] = clean_tweet_clm
-pd.set_option('display.max_columns', None)
-print(df.head())
-
-# remove stopwords
-stop_words = set(stopwords.words('english'))
-df['clean_tweet'] = df['clean_tweet'].apply(
-    lambda tweet: ' '.join([word for word in tweet.split() if not word in stop_words]))
-
-# lemmatization
-nltk.download('wordnet')
-lemmatizer = WordNetLemmatizer()
-df['clean_tweet'] = df['clean_tweet'].apply(
-    lambda tweet: ' '.join([lemmatizer.lemmatize(word) for word in tweet.split()]))
-
-# stem
-ps = PorterStemmer()
-df['clean_tweet'] = df['clean_tweet'].apply(
-    lambda tweet: ' '.join([ps.stem(word) for word in tweet.split()]))
-
-# remove hashtag
-df['clean_tweet'] = df['clean_tweet'].apply(
-    lambda tweet: ' '.join([word.replace('#','') if word.startswith('#') else word for word in tweet.split() ]))
-
-# handle some NaN values
-df.replace("", float("NaN"), inplace=True)
-df.dropna(subset=['clean_tweet'], inplace=True)
-is_NaN = df.isnull()
-row_has_NaN = is_NaN.any(axis=1)
-rows_with_NaN = df[row_has_NaN]
-
-#save to file
-df.to_csv('../../data/twitter_dataset_after_cleaning.csv')
-
-print("---end of cleaning---")
-
-df = pd.read_csv('../../data/twitter_dataset_after_cleaning.csv')
+df = pd.read_csv('../../data/twitter_hatespeech_dataset_after_cleaning.csv')
 
 df = df.iloc[:,[2,4]]
 
